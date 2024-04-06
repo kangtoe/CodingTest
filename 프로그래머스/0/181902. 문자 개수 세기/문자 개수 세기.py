714d0b15fb7e99f90947856bacf39f668241fd78
kangtoe <kangtoe99@gmail.com>
2024-04-06 22:55:57 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.01 ms, Memory: 9.98 MB -BaekjoonHub

--- 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.py
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.py
@@ -1,10 +1,12 @@
 def solution(my_string):
     answer = list(0 for i in range(52))    
-
+    alp = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"    
     for s in my_string:
-        if s <= 'Z': 
-            answer[ord(s) - 65] += 1
-        else: 
-            answer[ord(s) - 65 - 32 + 26] += 1
+        answer[alp.index(s)] += 1
+        
+        #if s <= 'Z': 
+            #answer[ord(s) - 65] += 1
+        #else: 
+            #answer[ord(s) - 65 - 32 + 26] += 1                    
     
     return answer
