f47e69772819524e42018ef2f410c3c5ea53e990
kangtoe <kangtoe99@gmail.com>
2024-04-06 22:19:46 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.py
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.py
@@ -1,16 +1,6 @@
 def solution(str_list):
-    
-    lFirst = False
-    idx = -1
     for i, s in enumerate(str_list):
-        if s == 'l':
-            lFirst = True
-            idx = i
-            break
-        if s == 'r':
-            lFirst = False
-            idx = i
-            break
-            
-    if idx == -1: return []
-    return str_list[:idx] if lFirst else str_list[idx+1:]
+        if s == 'l': return str_list[:i] 
+        if s == 'r': return str_list[i+1:]
+
+    return []
