8e717e8e24268f8f9681e38d296976c03a6b563e
kangtoe <kangtoe99@gmail.com>
2024-04-06 19:46:01 +0900

    [level 0] Title: 정사각형으로 만들기, Time: 0.00 ms, Memory: 11.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.py
@@ -0,0 +1,20 @@
+def solution(arr):
+    answer = [[]]
+    
+    col = len(arr[0])
+    row = len(arr)    
+    i = col - row
+    
+    if i > 0:
+        z = []
+        for j in range(col):
+            z.append(0)
+        
+        for j in range(i):
+            arr.append(z)
+        
+    elif i < 0:
+        for a in arr:
+            for j in range(-i):
+                a.append(0)        
+    return arr
