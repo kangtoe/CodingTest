e7161f440448cd750496cfd1cbee5221953b7636
kangtoe <kangtoe99@gmail.com>
2024-04-06 23:40:36 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.py
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.py
@@ -1,12 +1,11 @@
 def solution(n):
-    count = 0
-    i = 0
+    count = 0 # 실제 숫자
+    i = 0 # 3x 숫자
     while True:        
-        i += 1
-        
+        i += 1        
         if i % 3 == 0 or '3' in str(i):
             continue
         else:
-            count += 1
-        
-        if count == n: return i
+            count += 1        
+        if count == n: 
+            return i
