77729d310e41cc222d6e3e63e665f739585557bd
kangtoe <kangtoe99@gmail.com>
2024-04-07 20:32:03 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -8,10 +8,6 @@ def to2(n):
 
 def solution(n):
     count1 = to2(n).count("1")
-    #print(count1)
-    i = n
-    while True:
-        i += 1
-        #print(i, to2(i))
+    for i in range(n+1, n*2+1):
         if count1 == to2(i).count("1"):
-            return i
+                return i
