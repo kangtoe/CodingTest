da8563126f4946d7148c0ff3b2d9558a67b70f95
kangtoe <kangtoe99@gmail.com>
2024-04-12 12:14:44 +0900

    [level 0] Title: 가까운 수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120890. 가까운 수/가까운 수.py
+++ 프로그래머스/0/120890. 가까운 수/가까운 수.py
@@ -1,13 +1,3 @@
 def solution(array, n):
-    close = 1e9
-    ran = 1e9
-    for i in array:
-        if abs(i - n) <= ran:
-            if abs(i - n) == ran and i > close:
-                continue
-                
-            close = i
-            ran = abs(i - n)
-            print(close, ran)
-                
-    return close
+    answer = 0
+    return answer
