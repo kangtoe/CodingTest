aec0e60184b4ca73c3774941cf94dc8a5d30640a
kangtoe <kangtoe99@gmail.com>
2024-04-15 09:57:35 +0900

    [level 2] Title: 피보나치 수, Time: 19.29 ms, Memory: 13.8 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -1,11 +1,10 @@
 def solution(n):
-    answer = 0
     arr = list(range(n+1))  
     
     arr[0] = 0
     arr[1] = 1        
         
     for i in range(2, n+1):
-        arr[i] = arr[i-1] + arr[i-2]
+        arr[i] = (arr[i-1] + arr[i-2]) % 1234567
                 
-    return arr[n] % 1234567
+    return arr[n] 
