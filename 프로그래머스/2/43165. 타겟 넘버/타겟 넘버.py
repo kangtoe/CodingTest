e1b2c641bf26e0e3ef586fb427384cf254e5a97c
kangtoe <kangtoe99@gmail.com>
2024-06-14 22:24:05 +0900

    [level 2] Title: 타겟 넘버, Time: 2.90 ms, Memory: 9.96 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,21 +1,17 @@
-# 현재 인덱스와 이전 단계까지의 계산값 인자 추가
-def func(numbers, target, idx, value): 
-    global cnt
-    
-    # 끝까지 연산 후 목표값과 동일
+def func(numbers, target, idx, value):
     if idx == len(numbers):
         if value == target: 
-            cnt += 1
+            answer += 1
         return
     
-    # +/- 경우에 따른 재귀적 호출
     func(numbers, target, idx + 1, value + numbers[idx])
     func(numbers, target, idx + 1, value - numbers[idx])
     
 
-def solution(numbers, target):    
-    global cnt
-    cnt = 0
+def solution(numbers, target):
+    global answer 
+    answer = 0
+    
     func(numbers, target, 0, 0)
     
-    return cnt
+    return answer
