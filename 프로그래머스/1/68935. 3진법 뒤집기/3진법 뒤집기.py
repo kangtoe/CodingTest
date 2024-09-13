041cacb441920de9fea6f6c703b4792f10a6aeef
kangtoe <kangtoe99@gmail.com>
2024-09-13 22:12:19 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
@@ -1,19 +1,18 @@
-def to3(i):
+def to3(i):         
     s = ''    
     jari = 0
-    while i > 0:
-        na = i % (3 ** (jari+1))
-        i -= na
-        s = str(na // 3 ** jari) + s                
-        jari += 1
-                        
+    while i > 0:        
+        mok = i % 3 
+        nam = i // 3     
+        i = nam
+        s = str(mok) + s
+        
     return s
     
 def to10(i):
     sum = 0
     for j, s in enumerate(str(i)[::-1]):
         sum += int(s) * (3**j)
-        #print(s, j)
         
     return sum
 
@@ -21,6 +20,6 @@ def solution(n):
     n3 = to3(n)
     re = n3[::-1]
     n10 = to10(re)
-    #print(n3, re, n10)    
+    #print(n3, re, n10)        
     
     return n10
