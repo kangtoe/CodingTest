618986f277c1479a8b7726f95a83c44bb8c40fd6
kangtoe <kangtoe99@gmail.com>
2025-01-21 00:07:51 +0900

    Update 오픈채팅방.py
    다시 작성해본 자신의 코드로 수정

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.py
@@ -1,18 +1,23 @@
 def solution(record):
+    
     dic = {}
+    for r in record:
+        tmp = r.split(" ")
+        act, uid = tmp[0],tmp[1]
+        if not act == "Leave": 
+            nick = tmp[2]
+            dic[uid] = nick
+            
     arr = []
-    actions = {"Enter": "님이 들어왔습니다.", "Leave": "님이 나갔습니다."}
-
     for r in record:
         tmp = r.split(" ")
-        act, uid = tmp[0], tmp[1]
-        if act in ("Enter", "Change"):
-            dic[uid] = tmp[2]
-
-    for r in record:
-        tmp = r.split(" ")
-        act, uid = tmp[0], tmp[1]
-        if act in actions:
-            arr.append(f"{dic[uid]}{actions[act]}")
-
+        act, uid = tmp[0],tmp[1]        
+                
+        if act == "Enter":
+            s = dic[uid] + "님이 들어왔습니다."
+            arr.append(s)
+        if act == "Leave":
+            s = dic[uid] + "님이 나갔습니다."        
+            arr.append(s)
+            
     return arr
