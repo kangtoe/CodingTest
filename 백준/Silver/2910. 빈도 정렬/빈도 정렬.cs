0e4a64ffb315a3436657ade818c2161f7f92caac
Han Kang <kangtoe99@gmail.com>
2025-09-17 20:15:10 +0900

    [Silver III] Title: 빈도 정렬, Time: 60 ms, Memory: 6912 KB -BaekjoonHub

--- 백준/Silver/2910. 빈도 정렬/빈도 정렬.cs
+++ 백준/Silver/2910. 빈도 정렬/빈도 정렬.cs
@@ -1,59 +1,53 @@
 using System;
 using System.Collections.Generic;
-class HelloWorld {
-    
-  static int Cmp(List<int> a, List<int> b){
-      
-    int freq = b[1] - a[1];
-    if(freq!=0) return freq;
-    return a[2] - b[2];
-  }
-    
-  static void Main() {
-    
-    SortedDictionary<int,int> mp, mp_first = new SortedDictionary<int,int>();
-    
-    string[] str = Console.ReadLine().Split();
-    int n = int.Parse(str[0]);
-    int c = int.Parse(str[1]);
-    
-    int[] arr = new int[n];
-    string[] nums = Console.ReadLine().Split();
-    
-    Dictionary<int,int> freqDict = new Dictionary<int,int>();
-    Dictionary<int,int> orderDict = new Dictionary<int,int>();
-    
-    for(int i = 0; i< n; i++)
-    {
-        arr[i] = int.Parse(nums[i]);
-        
-        if(freqDict.ContainsKey(arr[i]))
-            freqDict[arr[i]]++;
-        else
-        {
-            freqDict[arr[i]] = 1;
-            orderDict[arr[i]] = i;
-        }
-    }
-    
-    List<List<int>> li = new List<List<int>>();
-    foreach(var kv in  freqDict)
-    {
-        int k = kv.Key;
-        int v = kv.Value;
-        int o = orderDict[k];
-        li.Add(new List<int>(){k,v,o});
-    }
-    
-    li.Sort(Cmp);
-    
-    foreach(List<int> l in li)
-    {
-        for(int i = 0; i< l[1]; i++)
-        {
-            Console.Write(l[0] + " ");
-        }
-        
-    }
-  }
+class HelloWorld 
+{
+	static void Main() 
+	{
+	    Dictionary<int,int> countDict = new Dictionary<int,int>();
+	    Dictionary<int,int> firstDict = new Dictionary<int,int>();
+	    
+	    string[] nc = Console.ReadLine().Split();
+	    int n= int.Parse(nc[0]);
+	    int c= int.Parse(nc[1]);
+	    
+	    string[] strs = Console.ReadLine().Split();
+	    for(int i = 0; i < n; i++)
+	    {
+	        int num = int.Parse(strs[i]);
+	        
+	        if(countDict.ContainsKey(num)) countDict[num]++;
+	        else countDict.Add(num, 1);
+	        
+	        if(!firstDict.ContainsKey(num)) firstDict.Add(num, i);
+	    }
+	    
+	    List<List<int>> arr = new List<List<int>>();
+	    foreach(int key in countDict.Keys)
+	    {
+	        List<int> li = new List<int>();
+	        li.Add(key);
+	        li.Add(countDict[key]);
+	        li.Add(firstDict[key]);
+	        arr.Add(li);
+	    }
+	    
+	    arr.Sort((a,b) =>{
+	        int count = b[1] - a[1];
+	        if(count == 0) return a[2] - b[2];
+	        return count;
+	    });
+	    
+	    List<int> ans = new List<int>();
+	    for(int i = 0; i < arr.Count; i++)
+	    {
+	        for(int j = 0; j < arr[i][1]; j++)
+	        {
+	            ans.Add(arr[i][0]);
+	        }
+	    }
+	    
+	    string s = string.Join(" ", ans);
+	    Console.Write(s);
+	}
 }
