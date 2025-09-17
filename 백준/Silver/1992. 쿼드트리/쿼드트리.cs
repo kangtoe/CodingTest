d6e861335da27f28779ac1c2f05e2fe43dff0694
Han Kang <kangtoe99@gmail.com>
2025-09-17 20:38:35 +0900

    [Silver I] Title: 쿼드트리, Time: 48 ms, Memory: 5952 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.cs
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.cs
@@ -1,49 +1,47 @@
 using System;
 class HelloWorld {
     
-    static string Quad(int y, int x, int size, int[,] grid)
+    static int n;
+    static int[,] grid;
+    
+    static string Dfs(int startY, int startX, int size)
     {
-        //Console.WriteLine(y + " " + x + " " +size);
-        if(size == 1) return grid[y,x].ToString();
+        if(size==1) return grid[startY,startX].ToString();
         
-        string str = "";
-        int c = grid[y,x];
-        for(int i = y; i < y + size; i++)
+        int first = grid[startY, startX];
+        for(int i = startY; i < startY + size; i++)
         {
-            for(int j = x; j < x + size; j++)
+            for(int j = startX; j < startX + size; j++)
             {
-                int h = size/2;
-                
-                if(grid[i,j]!=c)
+                if(grid[i,j]!=first)
                 {
+                    string str = "";
                     str += "(";
-                    str += Quad(y,x,h,grid);
-                    str += Quad(y,x+h,h,grid);
-                    str += Quad(y+h,x,h,grid);
-                    str += Quad(y+h,x+h,h,grid);
+                    str += Dfs(startY,startX,size/2);
+                    str += Dfs(startY,startX+size/2,size/2);
+                    str += Dfs(startY+size/2,startX,size/2);
+                    str += Dfs(startY+size/2,startX+size/2,size/2);
                     str += ")";
                     return str;
                 }
             }
         }
-        
-        return c.ToString();
+        return first.ToString();
     }
     
 	static void Main() {
-		int n = int.Parse(Console.ReadLine());
+		n = int.Parse(Console.ReadLine());
+		grid = new int[n,n];
 		
-		int[,] grid = new int[n,n];
-		for(int i = 0; i< n;i++)
+		for(int i = 0; i <n; i++)
 		{
 		    string str = Console.ReadLine();
-		    for(int j = 0; j < n;j++)
+		    for(int j = 0; j < n; j++)
 		    {
 		        grid[i,j] = str[j] - '0';
 		    }
 		}
 		
-		string ans = Quad(0,0,n,grid);
-		Console.WriteLine(ans);
+		Console.WriteLine(Dfs(0,0,n));
 	}
 }
