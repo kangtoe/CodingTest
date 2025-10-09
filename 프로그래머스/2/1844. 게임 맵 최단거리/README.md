745d2858ec862464335050e40aff8e79c3ec96b7
Han Kang <kangtoe99@gmail.com>
2025-10-09 18:30:49 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 1.99 ms, Memory: 32.5 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=csharp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
 
 ### 성능 요약
 
-메모리: 31.6 MB, 시간: 1.31 ms
+메모리: 32.5 MB, 시간: 1.99 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 04일 19:35:32
+2025년 10월 09일 18:30:46
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cs
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cs
@@ -2,40 +2,45 @@ using System;
 using System.Collections.Generic;
 
 class Solution {
-    public int solution(int[,] maps) {                                        
-        int n = maps.GetLength(0);
-        int m = maps.GetLength(1);
-        int[,] visited = new int[n,m];
+    public int solution(int[,] maps) {                
+        
+        int row = maps.GetLength(0);
+        int col = maps.GetLength(1);
+                
+        int[,] visited = new int[row,col];
         
         Queue<int> xq = new Queue<int>();
         Queue<int> yq = new Queue<int>();
         
-        int[] dx = new int[]{0,0,-1,1};
-        int[] dy = new int[]{1,-1,0,0};        
-        
         xq.Enqueue(0);
         yq.Enqueue(0);
         visited[0,0] = 1;
+        
         while(xq.Count > 0)
-        {            
+        {
             int x = xq.Dequeue();
-            int y = yq.Dequeue();                            
+            int y = yq.Dequeue();
+            
+            int[] dx = {0,0,-1,1};
+            int[] dy = {1,-1,0,0};
             
             for(int i = 0; i < 4; i++)
-            {                                                
+            {
                 int nx = x + dx[i];
                 int ny = y + dy[i];
                 
-                if(nx < 0 || ny < 0 || nx >= m || ny >= n) continue;
-                if(maps[ny, nx] == 0) continue;
-                if(visited[ny, nx]!=0) continue;
+                if(nx < 0 || ny < 0 || nx >= col || ny >= row) continue;
+                if(visited[ny,nx] != 0) continue;
+                if(maps[ny,nx] != 1) continue;
                 
+                visited[ny,nx] = visited[y,x] + 1;
                 xq.Enqueue(nx);
                 yq.Enqueue(ny);
-                visited[ny, nx] = visited[y, x] + 1;                                
-            }                        
-        }
-                
-        return visited[n-1,m-1] != 0 ? visited[n-1,m-1] : -1;
+            }
+        }        
+               
+        int res = visited[row-1,col-1];
+        if(res == 0) return -1;
+        else return res;
     }
 }
