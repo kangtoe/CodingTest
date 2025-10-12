fe5d3391af59ad055d01dad04ec40a87d6a07c6d
Han Kang <kangtoe99@gmail.com>
2025-10-12 23:34:11 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 48 ms, Memory: 5468 KB -BaekjoonHub

---
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cs
@@ -0,0 +1,15 @@
+using System;
+class HelloWorld {
+	static void Main() {
+	    string str = Console.ReadLine();
+		for(int i=0; i<str.Length/2; i++)
+		{
+		    if(str[i]!=str[str.Length-1-i])
+		    {
+		        Console.WriteLine(0);
+		        return;
+		    }
+		}
+		Console.WriteLine(1);
+	}
+}
