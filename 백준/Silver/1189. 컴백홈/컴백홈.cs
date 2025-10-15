e640e8f0d0f5caae21070215166afb1e071087e3
Han Kang <kangtoe99@gmail.com>
2025-10-15 23:20:10 +0900

    [Silver I] Title: 컴백홈, Time: 60 ms, Memory: 12948 KB -BaekjoonHub

--- 백준/Silver/1189. 컴백홈/컴백홈.cs
+++ 백준/Silver/1189. 컴백홈/컴백홈.cs
@@ -1,57 +1,72 @@
 using System;
 using System.Collections.Generic;
+
 class HelloWorld {
-    static int r, c, k;
-    static int count = 0;
+    static int ans = 0;
     
-    static void Go(int x, int y, int[,] visited, char[,] grid)
+    static void DFS(int y, int x, int[,] grid, int[,] visited, int r, int c, int k, int count)
     {
+        int[]dy={0,0,1,-1};
+        int[]dx={1,-1,0,0};
+        
         if(y==0&&x==c-1)
         {
-            //Console.WriteLine("len:" + visited[y,x]);
-            if(visited[y,x]==k) count++;
+            if(count==k) ans++;
+            return;
         }
-            
-        
-        int[] dy = new int[]{0,0,-1,1};
-        int[] dx = new int[]{1,-1,0,0};
         
         for(int i =0; i< 4; i++)
         {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
+            int ny = y +dy[i];
+            int nx = x +dx[i];
             
-            if(nx<0||ny<0||nx>=visited.GetLength(1)||ny>=visited.GetLength(0)) continue;
-            if(visited[ny,nx]!=0) continue;
-            if(grid[ny,nx]=='T') continue;
+            if(ny<0||nx<0||ny>=r||nx>=c) continue;
+            if(grid[ny,nx]!=0) continue;
+            if(visited[ny,nx]==1) continue;
             
-            visited[ny,nx] = visited[y,x] + 1;
-            Go(nx,ny, visited, grid);
+            visited[ny,nx] = 1;
+            DFS(ny,nx,grid,visited,r,c,k,count+1);
             visited[ny,nx] = 0;
         }
     }
     
+    static void print(int[,]arr)
+    {
+        Console.WriteLine("---");
+        for(int i =0; i< arr.GetLength(0); i++)
+        {
+            for(int j = 0; j < arr.GetLength(1); j++)
+            {
+                Console.Write(arr[i,j] + " ");
+            }
+            Console.WriteLine("");
+        }
+    }
+    
 	static void Main() {
+		int r,c,k;
 		string[] rck = Console.ReadLine().Split();
-		
 		r = int.Parse(rck[0]);
 		c = int.Parse(rck[1]);
 		k = int.Parse(rck[2]);
 		
-		char[,] grid = new char[r,c];
-		int[,] visited = new int[r,c];
-		
+		int [,] grid = new int[r,c];
+		int [,] visited = new int[r,c];
 		for(int i =0; i< r; i++)
 		{
-		    string str = Console.ReadLine();
-		    for(int j =0; j< c; j++)
+		    string line = Console.ReadLine();
+		    for(int j =0; j <c; j++)
 		    {
-		        grid[i,j] = str[j];
+		        if(line[j] == '.') grid[i,j] = 0;
+		        else grid[i,j] = 1;
 		    }
 		}
 		
-		visited[r-1,0] = 1;
-		Go(0,r-1,visited,grid);
-		Console.WriteLine(count);
+		int y = r-1;
+		int x = 0;
+		visited[y,x] = 1;
+		DFS(y,x,grid,visited,r,c,k,1);
+		
+		Console.WriteLine(ans);
 	}
 }
